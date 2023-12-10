dec78ddfcc5c8f34694b5894c6f6d51944eb4aab
devlopersabbir <devlopersabbir@gmail.com>
2023-12-10 14:18:43 +0600

    [feat 🤺] separate code with best practise

---
+++ constants/_data.py
@@ -0,0 +1,12 @@
+fake_items_db:list[dict] = [
+        {
+            "item_name": "Foo"
+        },
+        {
+            "item_name": "Bar"
+        },
+        {
+            "item_name": "Baz"
+        }
+    ]
+
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
+from db._data import fake_items_db
 
 app = FastAPI()
-fake_items_db:list[dict] = [
-        {
-            "item_name": "Foo"
-        },
-        {
-            "item_name": "Bar"
-        },
-        {
-            "item_name": "Baz"
-        }
-    ]
 
 @app.get("/")
 async def getPost(q:str, name: str = None):
@@ -23,7 +13,7 @@ async def getPost(q:str, name: str = None):
 async def read(skip: int = 0, limit:int = 10)->list[dict]:
     print(skip)
     print(limit)
-    print(fake_items_db[skip : skip + limit]) # spacially this line please explain me
+    print(fake_items_db[skip : skip + limit])
     return {
         "message": "hello"
     }
