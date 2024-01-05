94b6acda34032188dc2875be4cb83e6a492b1cbb
devlopersabbir <devlopersabbir@gmail.com>
2024-01-06 02:07:21 +0600

    [fix 📌] Body - Multiple Parameters

--- multiple.py
+++ multiple.py
@@ -1,3 +1,4 @@
+# from __future__ import annotations
 from fastapi import FastAPI, Path, Query
 from fastapi.responses import JSONResponse
 from typing import Annotated
@@ -8,16 +9,16 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
+    description: str or None = None
     price: float
-    tax: float | None = None
+    tax: float or None = None
 
 
 @app.put("/items/{item_id}")
 async def update_item(
     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str | None = None,
-    item: Item | None = None,
+    q: str or None = None,
+    item: Item or None = None,
 ):
     results = {"item_id": item_id}
     if q:
