1ad142439cd2e811a3509641dc93e6ebffbac6e8
devlopersabbir <devlopersabbir@gmail.com>
2024-01-11 04:14:42 +0600

    [feat 📌] multi params

--- multiple.py
+++ multiple.py
@@ -1,28 +1,31 @@
-# from __future__ import annotations
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Query, Path
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from typing import Annotated
 from pydantic import BaseModel
 
 app = FastAPI()
 
-
 class Item(BaseModel):
-    name: str
-    description: str or None = None
-    price: float
-    tax: float or None = None
+    name: str | None = None
+    username: str
+    password: str
+    age: int | None = 19
 
 
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-    q: str or None = None,
-    item: Item or None = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+@app.put("/{id}", tags=["update data"])
+async def read_root(id: Annotated[int, Path(title="product id", ge=0, le=100)], query: Annotated[str | None, Query(title="search query")] = None, item: Item | None = None):
+    print(id)
+    print(query)
+    print(item)
+    result = {"user_id": id}
+    if query:
+        result.update({
+            "query":query
+        })
     if item:
-        results.update({"item": item})
-    return results
+
+        result.update({
+            "item": jsonable_encoder(item) 
+        })
+    return JSONResponse(content=result, status_code=200)
